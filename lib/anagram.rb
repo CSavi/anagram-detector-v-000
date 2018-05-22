1584e6b1eb5ac7f8e8a9426c3f88d92f3c522a7e
Carrie Savinell <carrielane89@gmail.com>
2018-05-22 19:07:38 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,13 +4,13 @@ class Anagram
   attr_accessor :word
   
   
-  def initialize(word)
-    @word = word 
+  def initialize(an_word)
+    @word = an_word 
   end 
   
   def match(word_array)
     word_array.each do |word|
-      if word.split("").sort == self.word.split("").sort
+      if word.split("").sort == self.an_word.split("").sort
       word 
       end 
     end
